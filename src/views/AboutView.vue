a2df5f46e0ae0fba1878c568db75d56656c0e4f5
p-w-g <p.grzelczyk@gmail.com>
2024-05-18 14:33:14 +0200

    abstract descriptions into separate components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,50 +1,10 @@
+<script setup lang="ts">
+import Tekniskt from '../components/About/Tekniskt.vue'
+import Utrustning from '../components/About/Utrustning.vue'
+import Kartan from '../components/About/Kartan.vue'
+</script>
 <template>
-  <h2 class="text-h2">Bavaria 36 Holiday.</h2>
-  <h3 class="text-subtitle-1">Tillgänglig från Svinninge Marina.</h3>
-  <h3 class="text-subtitle-2">Tre hytt ger mycket utrymme för sällskåpet.</h3>
-  <v-expansion-panels>
-    <v-expansion-panel title="Tekniskt beskrivning">
-      <v-expansion-panel-text>
-        <v-list lines="one" density="compact">
-          <v-list-item>Längd 11.5m</v-list-item>
-          <v-list-item>Bredd 3.7mck</v-list-item>
-          <v-list-item>Djup 1.85m</v-list-item>
-          <v-list-item>Höjd från vattenytan: 16.5m</v-list-item>
-        </v-list>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-  </v-expansion-panels>
-
-  <v-expansion-panels>
-    <v-expansion-panel title="Utrustning">
-      <v-expansion-panel-text>
-        <v-list lines="one" density="compact">
-          <v-list-item>Rullmast</v-list-item>
-          <v-list-item>Självslående fock</v-list-item>
-          <v-list-item>Vindinstrument</v-list-item>
-          <v-list-item>Plotter</v-list-item>
-          <v-list-item>Bogpropeller</v-list-item>
-          <v-list-item>Elektriskt ankarspel med blyad lina</v-list-item>
-          <v-list-item>3 hytt med dubbla kojplatser</v-list-item>
-          <v-list-item>Stereo i salongen och i sittbrunnen</v-list-item>
-          <v-list-item>Eltoa</v-list-item>
-          <v-list-item>Grill (För användning på land)</v-list-item>
-          <v-list-item>Flytvästar</v-list-item>
-          <v-list-item>Lakan</v-list-item>
-          <v-list-item>Gasolspis</v-list-item>
-          <v-list-item>Kyl</v-list-item>
-          <v-list-item>Värmare</v-list-item>
-          <v-list-item>Kapell över hela sittbrunnen</v-list-item>
-          <v-list-item>Dusch på badbrygga (varm o kall)</v-list-item>
-          <v-list-item>Nya madrasser 2024</v-list-item>
-          <v-list-item>Gummibåt</v-list-item>
-          <v-list-item>Portabel VHF</v-list-item>
-          <v-list-item>mm</v-list-item>
-        </v-list>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-  </v-expansion-panels>
-  Båtens placering på Google Maps:
-  https://www.google.com/maps/place/Svinninge+Marina+AB/@59.4375201,18.0560766,10z/data=!4m6!3m5!1s0x465f85870ec81e6b:0x5508fd51ea5c6bff!8m2!3d59.4572983!4d18.2776362!16s%2Fg%2F1hc2_q_yr?authuser=0&entry=ttu
-  Tveka inte att kontakta mig vid frågor.
+  <Tekniskt />
+  <Utrustning />
+  <Kartan />
 </template>
