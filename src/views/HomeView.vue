58dbdee25994b8510d57903cff61bfc9b6a2459c
p-w-g <p.grzelczyk@gmail.com>
2024-05-18 14:32:31 +0200

    cleanup the home and implement hero and textblock

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HeroSection from '../components/HeroSection.vue'
+import TextBlock from '../components/TextBlock.vue'
 </script>
 
 <template>
-  <TheWelcome />
+  <HeroSection />
+  <TextBlock
+    >Att segla är att uppleva friheten på ett helt unikt sätt. Känn vinden i seglen, hör vågorna
+    klucka mot skrovet och njut av den orörda naturen som omger dig. Segling är inte bara en resa,
+    det är ett äventyr där varje dag bjuder på nya vyer och möjligheter. Upptäck skärgårdens gömda
+    pärlor, ankra i lugna vikar och låt solen gå ner över horisonten medan du njuter av tystnaden
+    och rofylldheten till havs. Vår segelbåt erbjuder en oförglömlig upplevelse för både nybörjare
+    och erfarna seglare. Kom och upplev magin av segling!</TextBlock
+  >
 </template>
