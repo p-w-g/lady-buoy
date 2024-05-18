cf849572639759007f0253a18ab2a343bd6c9bad
p-w-g <p.grzelczyk@gmail.com>
2024-05-18 14:32:39 +0200

    scaffold hero

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid class="hero-container">
+    <v-row align="center">
+      <v-col cols="12">
+        <div class="text-right">
+          <h1 class="display-2">Bavaria 36 Holiday</h1>
+          <p class="subtitle-1">Tre hytt ger mycket utrymme för sällskåpet</p>
+          <p class="subtitle-1">Tillgänglig från Svinninge Marina.</p>
+          Bavaria image here
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.hero-container {
+  background-size: cover;
+  background-position: center;
+  padding: 50px;
+}
+
+.hero-btn {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+}
+</style>
