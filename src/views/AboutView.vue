9ba920b61dab4591bc32960b544328d733855d7c
p-w-g <p.grzelczyk@gmail.com>
2024-05-18 16:54:09 +0200

    remove map from about-boat

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import Tekniskt from '../components/About/Tekniskt.vue'
 import Utrustning from '../components/About/Utrustning.vue'
-import Kartan from '../components/About/Kartan.vue'
 </script>
 <template>
   <h2 class="text-h2">Om båten</h2>
   <Tekniskt />
   <Utrustning />
-  <Kartan />
 </template>
