95af5d4374aa38c3080c11018a74a5a408aeffe0
p-w-g <p.grzelczyk@gmail.com>
2024-05-18 16:59:36 +0200

    bootstrap basic contact view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TextBlock from '../components/TextBlock.vue'
 </script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2 class="text-h2">Kontakta oss</h2>
+  <TextBlock>
+    <template #header>
+      <p class="subtitle-1">Skicka gärna ett meddelande vid funderingar</p>
+    </template>
+    <template #default>
+      <v-list lines="one" density="compact">
+        <v-list-item>Email?</v-list-item>
+        <v-list-item>Telefon?</v-list-item>
+      </v-list>
+    </template>
+  </TextBlock>
 </template>
