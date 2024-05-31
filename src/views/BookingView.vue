ec170dbe83d22d7d9df8d0c701af2fb7e95732a7
p-w-g <p.grzelczyk@gmail.com>
2024-05-31 18:00:11 +0200

    prune "card" background

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -8,37 +8,32 @@
   </v-container>
   <v-container class="minimal-container">
     <p class="subtitle-1">Pris och vilkor</p>
+    <br>
     <v-row no-gutters>
       <v-col cols="12" sm="6">
-        <v-sheet class="minimal-sheet">
-          <v-list lines="one" density="compact">
-            <v-list-item>18000kr/vecka (tom v33)</v-list-item>
-            <v-list-item>14000kr/vecka (v34 framåt)</v-list-item>
-            <v-list-item>Deposition : 10500kr </v-list-item>
-          </v-list>
-        </v-sheet>
+        <p>18000kr/vecka (tom v33)</p>
+        <p>14000kr/vecka (v34 framåt)</p>
+        <p>Deposition: 10500kr</p>
       </v-col>
 
       <v-col cols="12" sm="6">
-        <v-sheet class="minimal-sheet">
-          <div class="text-body-1">
-            <p>Kräver nautisk kompetens. Båten försäkrat hos Svenska Sjö.</p>
-            <p>Kontrakt enligt mall i länken. Rabatt vid längre uthyrning</p>
-            <br />
-            <p>
-              Deposition: 10500kr (Samma som självrisk - Återbetalas i helhet om båten lämnas
-              tillbaka i samma skick med full tank). 10% i handpenning för att reservera båten.
-              Resten senast vid avhämtning.
-            </p>
-            <br />
-            <p>
-              Kan teckna hyreskontraktet i förväg digitalt eller vid utlämning. Uthyres lördag till
-              lördag. Exakta tider enligt överenskommelse, men som tumregel avfärd på eftermiddag
-              och ankomst på förmiddag.
-            </p>
-            <p>Ring gärna vid funderingar.</p>
-          </div>
-        </v-sheet>
+        <div class="text-body-1">
+          <p>Kräver nautisk kompetens. Båten försäkrat hos Svenska Sjö.</p>
+          <p>Kontrakt enligt mall i länken. Rabatt vid längre uthyrning</p>
+          <br />
+          <p>
+            Deposition: 10500kr (Samma som självrisk - Återbetalas i helhet om båten lämnas tillbaka
+            i samma skick med full tank). 10% i handpenning för att reservera båten. Resten senast
+            vid avhämtning.
+          </p>
+          <br />
+          <p>
+            Kan teckna hyreskontraktet i förväg digitalt eller vid utlämning. Uthyres lördag till
+            lördag. Exakta tider enligt överenskommelse, men som tumregel avfärd på eftermiddag och
+            ankomst på förmiddag.
+          </p>
+          <p>Ring gärna vid funderingar.</p>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -53,10 +48,5 @@
 
 .minimal-sheet {
   padding: 16px;
-  margin: 16px;
-  background-color: white;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  border: 1px solid #e0e0e0;
 }
 </style>
