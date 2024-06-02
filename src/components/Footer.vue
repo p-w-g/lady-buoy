ccbbbf1fbf7d2f99a5172c7f2efe300ab739d026
p-w-g <p.grzelczyk@gmail.com>
2024-06-02 09:57:42 +0200

    bootstrap a footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-footer class="bg-grey-lighten-1">
+    <v-row justify="center" no-gutters>
+      <v-row align="center">
+        <v-col cols="12">
+          <div class="text-right">
+            <h1 class="display-2">Sail Yachtsea</h1>
+            <p class="subtitle-1">
+              <span>Sailyachtsea@gmail.com</span>
+            </p>
+            <p class="subtitle-1">
+              <span>0768498391</span>
+            </p>
+            <p class="subtitle-1">Godkänd för F-Skatt</p>
+          </div>
+        </v-col>
+      </v-row>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Sail Yachtsea</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
